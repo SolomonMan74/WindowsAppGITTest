e7c6e0ddacef9d0f92abf5c9634adc57652a0bfc
Chris Gray <Chris.Gray@grammer.com>
2021-02-11 13:44:29 -0500

    Test 345

--- WindowsAppGITTest/Form1.vb
+++ WindowsAppGITTest/Form1.vb
@@ -6,5 +6,6 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         MessageBox.Show("The GIT Test")
         MessageBox.Show("The GIT Test 2")
+        MessageBox.Show("This is a real test")
     End Sub
 End Class
